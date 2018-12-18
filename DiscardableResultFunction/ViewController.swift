22bf4a32b05e02481bd56ec6013749aa24c71f60
Alex <therebeloper@gmail.com>
2018-12-18 14:15:13 +0200

    first commit

--- DiscardableResultFunction/ViewController.swift
+++ DiscardableResultFunction/ViewController.swift
@@ -13,8 +13,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let value = 320
+        let key = "percentage"
+        
+//        let result = set(value: value, forKey: key)
+//
+//        if result {
+//            let percentage = get(forKey: key)
+//            print("Percentage: \(percentage)")
+//        } else {
+//            print("Tried to set invalid percentage")
+//        }
+        
+        if set(value: value, forKey: key) {
+            let percentage = get(forKey: key)
+            print("Percentage: \(percentage)")
+        } else {
+            print("Tried to set invalid percentage")
+        }
+        
     }
-
-
+    
+    @discardableResult
+    func set(value: Int, forKey key: String) -> Bool {
+        if value >= 0 && value <= 100 {
+            UserDefaults.standard.set(value, forKey: key)
+            UserDefaults.standard.synchronize()
+            return true
+        }
+        return false
+    }
+    
+    func get(forKey key: String) -> Int {
+        return UserDefaults.standard.integer(forKey: key)
+    }
 }
 
